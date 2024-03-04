0a97a5b3745a876ae7458ed6d660526a7eb0bd96
izzyjosh <calebayuba222@gmail.com>
2024-03-04 23:07:43 +0100

    Added location getter

--- account/views.py
+++ account/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render,redirect
 from django.http import HttpRequest
 from .forms import UserRegistrationForm
+from django.contrib.auth import authenticate
+from main.models import Location
+
+from opencage.geocoder import OpenCageGeocode
+from django.conf import settings
+
+key = settings.GEOCODER_API
+geocoder = OpenCageGeocode(key)
+
+results = geocoder.reverse_geocode(44.8303087, -0.5761911)
+print(results[0]["formatted"])
 
 def signup(request:HttpRequest):
     if request.method == "POST":
@@ -14,4 +25,20 @@ def signup(request:HttpRequest):
         form = UserRegistrationForm()
 
     return render(request,"account/signup.html",{"form":form})
+
+def signin(request:HttpRequest):
+    if request.method == "POST":
+       username =  request.POST.get("username")
+       password = request.POST.get("password")
+
+       longitude = request.POST.get("longitude")
+       latitude = request.POST.get("latidude")
+
+       user = authenticate(request,username=username,password=password)
+
+       if user is not None:
+           login(request,user)
+           location = geocoder.reverse_geocode(logitude,latitude)
+           Location.objects.create(user=request.user,location=location)
+
 
--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Location
 
-# Create your views here.
+
+def dashboard(request):
+    locations = Location.objects.all()[:5]
+    print(locations)
+    return render(request,"dashboard.html",context={"locations":locations})
